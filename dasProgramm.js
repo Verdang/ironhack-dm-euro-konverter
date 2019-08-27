625086f3eed3adcf1ed3cd2bdd66639ef3d067ee
Verdang <verdang@media-saturn.com>
2019-08-27 15:42:54 +0200

    neues Projekt mit 2 Datein hochgeladen

---
+++ dasProgramm.js
@@ -0,0 +1,37 @@
+
+let accountObjectArray = [
+    {
+        kontoInhaberString: "Jakob",
+        kontoStandNumber: -10
+    },
+    {
+        kontoInhaberString: "Jürgen",
+        kontoStandNumber: 20
+    },
+    {
+        kontoInhaberString: "Patrick",
+        kontoStandNumber: -12844
+    }    
+];
+
+const wechselKursNumber = 1.9558;
+/*
+function convertDmToEuro(EinDMBankkontoObjectArray){
+    console.log(EinDMBankkontoObjectArray);
+const ergebnisArray = Array.from(EinDMBankkontoObjectArray);
+return ergebnisArray;
+}
+*/
+
+function convertDmToEuro(EinDMBankkontoObjectArray){
+    console.log(EinDMBankkontoObjectArray);
+const ergebnisArray = Array.from(EinDMBankkontoObjectArray);
+return ergebnisArray;
+}
+
+
+
+//convertDmToEuro(accountObjectArray);
+
+
+
